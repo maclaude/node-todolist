14abdc1d7a3f1eb28b36f48e04edbe69c315c56f
Marc-Antoine CLAUDE <claude.marcantoine2@gmail.com>
2023-05-15 19:06:13 +0200

    setup eslint & prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import bodyParser from "body-parser";
-import compression from "compression";
-import dotenv from "dotenv";
-import express from "express";
-import helmet from "helmet";
-import mongoose from "mongoose";
-import morgan from "morgan";
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
 
-import authRoutes from "./routes/auth";
+import authRoutes from './routes/auth';
 
 // Environment variables (.env)
 dotenv.config();
@@ -20,18 +20,18 @@ app.use(bodyParser.json());
 
 // Set responses CORS headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE',
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   next();
 });
 
 // Request logger
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 // Initialize helmet (set HTTP response header for security purpose)
 app.use(helmet());
@@ -40,30 +40,27 @@ app.use(helmet());
 app.use(compression());
 
 // Routes
-app.use("/auth", authRoutes);
+app.use('/auth', authRoutes);
 
-app.get("/", (req, res) => {
-  res.json({title: "Hello World !"});
+app.get('/home', (req, res) => {
+  res.json({ title: 'Hello World !' });
 });
 
 /**
  * Database connexion with Mongoose
  */
 // Database env variables
-const {
-  MONGO_DB_USER,
-  MONGO_DB_PASSWORD,
-} = process.env;
+const { MONGO_DB_USER, MONGO_DB_PASSWORD } = process.env;
 
 // Database URI
-const DB_URI = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@cluster-node-todolist.msl8yjl.mongodb.net/`
+const DB_URI = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@cluster-node-todolist.msl8yjl.mongodb.net/`;
 
 mongoose
   .connect(DB_URI)
-  .then(response => {
+  .then(() => {
     console.log('Connected');
 
     // Starting the server
     app.listen(process.env.PORT || 3000);
   })
-  .catch(error => console.log(error));
+  .catch((error) => console.log('Error:', error));
