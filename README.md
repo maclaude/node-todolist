c5f625d836c1811d3b6a1763c5395fea5ef787ef
Marc-Antoine CLAUDE <claude.marcantoine2@gmail.com>
2023-05-15 12:35:41 +0200

    add dependencies and set folders structure

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # node-todolist
+
+## Dependencies
+
+- [express](https://www.npmjs.com/package/express)
+- [dotenv](https://www.npmjs.com/package/dotenv)
+- [bodyParser](https://www.npmjs.com/package/body-parser)
+- [compression](https://www.npmjs.com/package/compression)
+- [helmet](https://www.npmjs.com/package/helmet)
+- [morgan](https://www.npmjs.com/package/morgan)
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,43 @@
+import bodyParser from "body-parser";
+import compression from "compression";
+import dotenv from "dotenv";
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 
+// Environment variables (.env)
+dotenv.config();
+
+// Initialize express
 const app = express();
 
+// Parser (parsing the incoming JSON data)
+// ! This middleware should always be placed first
+app.use(bodyParser.json());
+
+// Set responses CORS headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
+
+// Request logger
+app.use(morgan("dev"));
+
+// Initialize helmet (set HTTP response header for security purpose)
+app.use(helmet());
+
+// Compress all responses
+app.use(compression());
+
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.json({title: "Hello World !"});
 });
 
 const port = process.env.PORT || 3000;
